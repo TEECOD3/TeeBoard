af8bd14925e58e406e70098b07ec50507311ed82
david temitope <temi236topemicheal@gmail.com>
2024-06-07 05:37:37 +0100

    Feature:completing the dashboard and adding auth

--- app/(Auth)/page.tsx
+++ app/(Auth)/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="bg-dark-blue">
       <div className="flex container min-h-screen items-center justify-center ">
-        <div className="py-8 px-6 lg:py-16 lg:px-8 w-[23rem] lg:w-[25rem] border-cyan-blue bg-white shadow-md rounded-lg">
+        <div className="py-8 px-6 lg:py-16 lg:px-8 w-[25rem] border-cyan-blue bg-white shadow-md rounded-lg">
           <LoginForm />
         </div>
       </div>
